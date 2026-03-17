cba74fcfe357ed8fecaa8e5af3324cad1f51e169
lance aspilla <lance.aspilla14@gmail.com>
2026-03-17 17:03:50 +0800

    Fix of start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Custom entrypoint script to start PHP-FPM and Apache
 echo "Starting PHP-FPM and Apache server..."
-php-fpm &
-exec apache2-foreground
+php-fpm -D
+apache2ctl -D FOREGROUND
